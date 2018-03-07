05d66a1397cf1517e71c7e6c69edc1a68b7225b8
TheRo <TheRo@DESKTOP-G1LO41A>
2018-03-07 02:09:52 -0500

    Add project files.

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblUnits = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.lblDay = New System.Windows.Forms.Label()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblResults = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblUnits
+        '
+        Me.lblUnits.Location = New System.Drawing.Point(34, 35)
+        Me.lblUnits.Name = "lblUnits"
+        Me.lblUnits.Size = New System.Drawing.Size(100, 23)
+        Me.lblUnits.TabIndex = 0
+        Me.lblUnits.Text = "Units:"
+        Me.lblUnits.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(126, 37)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(122, 20)
+        Me.txtInput.TabIndex = 1
+        Me.txtInput.Text = " "
+        '
+        'lblDay
+        '
+        Me.lblDay.Location = New System.Drawing.Point(254, 37)
+        Me.lblDay.Name = "lblDay"
+        Me.lblDay.Size = New System.Drawing.Size(53, 20)
+        Me.lblDay.TabIndex = 2
+        Me.lblDay.Text = "Day: 1"
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(70, 81)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ReadOnly = True
+        Me.txtOutput.Size = New System.Drawing.Size(237, 177)
+        Me.txtOutput.TabIndex = 3
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(70, 289)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 4
+        Me.btnEnter.Text = "&Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(151, 289)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 5
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(232, 289)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblResults
+        '
+        Me.lblResults.Location = New System.Drawing.Point(70, 261)
+        Me.lblResults.Name = "lblResults"
+        Me.lblResults.Size = New System.Drawing.Size(237, 25)
+        Me.lblResults.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(383, 337)
+        Me.Controls.Add(Me.lblResults)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.lblDay)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.lblUnits)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblUnits As Label
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents lblDay As Label
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblResults As Label
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,112 @@
+﻿Public Class Form1
+    'Variables
+    'This is the minimum value integer for the range
+    Const minValue As Integer = 0
+    'This is the maximum value integer for the range
+    Const maxValue As Integer = 1000
+    'Number of days in a week
+    Const daysInAWeek As Integer = 7
+    'Units each day
+    Dim unitsEachDay(6) As Integer
+    Dim counter As Integer = 0
+    'Calculates average
+    Dim average As Double
+    Dim total As Integer
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        'Reset all inputs
+        txtInput.Text = ""
+        txtOutput.Text = ""
+        lblDay.Text = "Days: 1"
+        lblResults.Text = ""
+        btnEnter.Enabled = True
+        counter = 0
+        average = 0
+        total = 0
+        txtInput.Focus()
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles txtOutput.TextChanged
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub txtUnits_Click(sender As Object, e As EventArgs) Handles lblUnits.Click
+
+    End Sub
+
+    Private Sub txtDay_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+
+
+
+
+
+
+        'Error Messages
+        If (Integer.TryParse(txtInput.Text, unitsEachDay(counter)) = False) Then
+            MessageBox.Show("The value must be a whole number", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error)
+            txtInput.Text = ""
+            txtInput.Focus()
+
+        Else
+            If ((unitsEachDay(counter) <= maxValue And unitsEachDay(counter) >= minValue) = False) Then
+                MessageBox.Show("The value must be within range of 0 and 1000", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error)
+                txtInput.Text = ""
+                txtInput.Focus()
+
+            Else
+                unitsEachDay(counter) = Convert.ToInt32(txtInput.Text)
+                txtOutput.Text += unitsEachDay(counter) & vbCrLf
+                lblDay.Text = "Days: " & counter + 1
+                counter += 1
+                txtInput.Text = ""
+                'Calculation
+
+                If (counter = daysInAWeek) Then
+                    btnEnter.Enabled = False
+
+                    For counter = 1 To daysInAWeek - 1
+                        total += unitsEachDay(counter)
+                    Next
+                    average = total / daysInAWeek + 1
+
+
+                    lblResults.Text = "Average per day: " & average
+
+                End If
+
+            End If
+
+        End If
+
+
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub txtInput_TextChanged(sender As Object, e As EventArgs) Handles txtInput.TextChanged
+
+    End Sub
+End Class
